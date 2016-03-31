0c5db89c0a5d632da4997d1c9a38181920ee90a8
JoeButikofer <joebutikofer@gmail.com>
2016-03-31 16:08:12 +0200

    add compass

--- app/src/main/java/ch/hes_so/glassrally/RallyAdapter.java
+++ app/src/main/java/ch/hes_so/glassrally/RallyAdapter.java
@@ -5,42 +5,45 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
 
-import com.google.android.glass.app.Card;
 import com.google.android.glass.widget.CardBuilder;
 import com.google.android.glass.widget.CardScrollAdapter;
 
 import java.util.List;
+
+import ch.hes_so.glassrally.compass.CompassView;
 
 public class RallyAdapter extends CardScrollAdapter
 {
     private List<Reward> mRewards;
     private Context mContext;
 
+    private CompassView mCompassView;
+
     public RallyAdapter(Context context, List<Reward> rewards) {
         mContext = context;
         mRewards = rewards;
+        mCompassView = new CompassView(context);
     }
 
     @Override
     public int getCount() {
-        return mRewards.size(); // + 1;  //TODO
+        return mRewards.size() + 1;
     }
 
     @Override
     public Object getItem(int i) {
         if(i == 0)
-            return null; //TODO return Arrow
+            return null; //TODO return something ?
         else
-            return mRewards.get(i /*- 1*/);  //TODO
+            return mRewards.get(i - 1);
     }
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        //Card card = new Card(context);
 
-        /*if(i > 0)*/ { //TODO
+        if(i > 0) {
 
-            Reward reward = mRewards.get(i); // mRewards.get(i-1);
+            Reward reward = mRewards.get(i-1);
 
             View rewardView = new CardBuilder(mContext, CardBuilder.Layout.EMBED_INSIDE)
                     .setEmbeddedLayout(R.layout.reward_layout)
@@ -48,20 +51,32 @@ public class RallyAdapter extends CardScrollAdapter
                     .getView();
 
             WebView webView = (WebView) rewardView.findViewById(R.id.webview);
-            //webView.loadData(reward.getContent(), "text/html", "utf-8");
             webView.loadUrl(reward.getContent());
+
+            //For scaling the content but doesn't work
             webView.getSettings().setLoadWithOverviewMode(true);
             webView.getSettings().setUseWideViewPort(true);
+
             return rewardView;
         }
-
-        //return null;
+        else
+        {
+            return mCompassView.getCompassView();
+        }
     }
 
     @Override
     public int getPosition(Object o) {
 
-        //TODO check if Arrow
-        return mRewards.indexOf(o);
+        if(o instanceof Reward)
+            return mRewards.indexOf(o);
+        else
+            return  0; //TODO maybe check more ?
+    }
+
+    public void addReward(Reward reward)
+    {
+        mRewards.add(0, reward);
+        notifyDataSetChanged();
     }
 }
---
+++ app/src/main/java/ch/hes_so/glassrally/compass/CompassView.java
@@ -0,0 +1,96 @@
+package ch.hes_so.glassrally.compass;
+
+import android.content.Context;
+import android.hardware.SensorManager;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+
+import com.google.android.glass.widget.CardBuilder;
+
+import ch.hes_so.glassrally.R;
+
+public class CompassView implements OrientationManager.OnChangedListener
+{
+    private View mCompassView;
+    private ImageView mCompassImageView;
+
+    private float mTargetDegree = 0f;
+    private float mCurrentDegree = 0f;
+
+    private OrientationManager mOrientationManager;
+
+    public CompassView(Context context) {
+
+        mCompassView = new CardBuilder(context, CardBuilder.Layout.EMBED_INSIDE)
+                .setEmbeddedLayout(R.layout.compass_layout)
+                .getView();
+
+        mCompassImageView = (ImageView) mCompassView.findViewById(R.id.imageViewCompass);
+
+        SensorManager sensorManager =
+                (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+
+        mOrientationManager = new OrientationManager(sensorManager);
+        mOrientationManager.addOnChangedListener(this);
+        startOrientationManager();
+    }
+
+    public View getCompassView()
+    {
+        return mCompassView;
+    }
+
+    public void setTargetDegree(float targetDegree)
+    {
+        mTargetDegree = targetDegree;
+        updateOrientation();
+    }
+
+    public void startOrientationManager()
+    {
+        mOrientationManager.start();
+    }
+
+    public void stopOrientationManager()
+    {
+        mOrientationManager.stop();
+    }
+
+    @Override
+    public void onOrientationChanged(OrientationManager orientationManager) {
+        updateOrientation();
+    }
+
+    @Override
+    public void onAccuracyChanged(OrientationManager orientationManager) {
+        //nothing
+    }
+
+    private void updateOrientation()
+    {
+        // get the angle around the z-axis rotated
+        float degree = Math.round(mOrientationManager.getHeading());
+
+        //TODO do something with the target degree
+
+        // create a rotation animation (reverse turn degree degrees)
+        RotateAnimation ra = new RotateAnimation(
+                mCurrentDegree,
+                -degree,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF,
+                0.5f);
+
+        // how long the animation will take place
+        ra.setDuration(210);
+
+        // set the animation after the end of the reservation status
+        ra.setFillAfter(true);
+
+        // Start the animation
+        mCompassImageView.startAnimation(ra);
+        mCurrentDegree = -degree;
+    }
+}
